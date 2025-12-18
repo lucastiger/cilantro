a68dee3ae10fa785472c85f48a6dfda1fd7e7a4f
lucastiger <116535626+lucastiger@users.noreply.github.com>
2025-12-17 18:04:57 -0800

    Create seq_utils.py

---
+++ utils/seq_utils.py
@@ -0,0 +1,37 @@
+# utils/seq_utils.py
+import numpy as np
+
+AA = "ACDEFGHIKLMNPQRSTVWY"
+PAD_ID = 0
+UNK_ID = 1
+START_ID = 2
+
+def build_vocab_and_encode(seqs, max_len=200):
+    aa_to_id = {aa:i+3 for i, aa in enumerate(AA)}
+    vocab = {"PAD":PAD_ID, "UNK":UNK_ID, "START":START_ID}
+    vocab.update({aa: aa_to_id[aa] for aa in AA})
+    enc = []
+    for s in seqs:
+        ids = [START_ID]
+        for ch in s[:max_len-1]:
+            ids.append(aa_to_id.get(ch, UNK_ID))
+        while len(ids) < max_len:
+            ids.append(PAD_ID)
+        enc.append(ids)
+    return np.array(enc, dtype=np.int32), vocab
+
+def decode_sequence_from_ids(ids):
+    inv = {0:"-", 1:"X", 2:""}
+    aa_list = list(AA)
+    for i, ch in enumerate(aa_list):
+        inv[i+3] = ch
+    seq = "".join(inv.get(int(x), "X") for x in ids)
+    seq = seq.replace("-", "")
+    return seq
+
+def load_fasta_as_sequences(path):
+    seqs = []
+    from Bio import SeqIO
+    for rec in SeqIO.parse(path, "fasta"):
+        seqs.append(str(rec.seq).upper())
+    return seqs
